5eb3fbec5874aaa886935075f3b28ee693dc87bf
Mitchell Blom <mitchellblom@gmail.com>
2017-03-08 13:04:26 -0600

    stuck on getting currentOz.name to log

--- scripts.js
+++ scripts.js
@@ -3,18 +3,23 @@ var dogFoodContainer = document.getElementById("dog-food-container");
 function makeDom(xhrData) { //comes from makeDom(data) in the executeThisCodeAfterFileLoaded function
     var dogFoodString = "";
     var currentDogFood;
-    var currentType;
+    var currentOz;
+
     for (var i = 0; i < xhrData.dog_brands.length; i++) {
         currentDogFood = xhrData.dog_brands[i];
-        dogFoodString += `<div>${currentDogFood.name}</div>`
+        dogFoodString += `<div>${currentDogFood.name}</div>`;
         for (var j = 0; j < xhrData.dog_brands[i].types.length; j++) {
-        	dogFoodString += `<li>${xhrData.dog_brands[i].types[j].type}<li>`
-	        for (var k = 0; k < xhrData.dog_brands[i].types[j].type.length; k++) {
-	        	console.log("third for loop: ", xhrData.dog_brands[i].types[j].volumes[k].name);
-	        	// add .name
-	        }
+            dogFoodString += `<li>${xhrData.dog_brands[i].types[j].type}<li>`;
+            for (var k = 0; k < xhrData.dog_brands[i].types[j].type.length; k++) {
+                currentOz = xhrData.dog_brands[i].types[j].volumes[k];
+                console.log("currentOz", currentOz); // adding .name to currentOz results in "cannot read property of undefined"
+                // for (var l = 0; l < xhrData.dog_brands[i].types[j].type[k].length; l++) {
+                //     console.log("currentOz", currentOz);	
+                //     // console.log("third for loop: ", currentOz.name);			
+                //     // dogFoodString += `<li>${currentOz.name}</li>`;
+                // }
+            }
         }
-        dogFoodString += ``
         dogFoodString += ``
         dogFoodString += ``
         dogFoodString += ``
@@ -23,7 +28,6 @@ function makeDom(xhrData) { //comes from makeDom(data) in the executeThisCodeAft
 
     dogFoodContainer.innerHTML = dogFoodString;
 }
-
 
 function executeThisCodeAfterFileLoaded() {
     // console.log("Data returned: ", Date.now());
