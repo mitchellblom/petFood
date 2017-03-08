b1184e72049852d71fd4ab91c5b27338b3ecb6fc
Mitchell Blom <mitchellblom@gmail.com>
2017-03-08 07:37:24 -0600

    changes from yesterday

--- scripts.js
+++ scripts.js
@@ -3,17 +3,24 @@ var dogFoodContainer = document.getElementById("dog-food-container");
 function makeDom(xhrData) { //comes from makeDom(data) in the executeThisCodeAfterFileLoaded function
     var dogFoodString = "";
     var currentDogFood;
+    var currentType;
     for (var i = 0; i < xhrData.dog_brands.length; i++) {
         currentDogFood = xhrData.dog_brands[i];
 
+      	// console.log(xhrData.dog_brands[i].type.length);
+
+        dogFoodString += ``
+        dogFoodString += ``
+        dogFoodString += `<div> ${currentDogFood.name} </div>`
+        for (var j = 0; j < xhrData.dog_brands[i].types.length; j++) {
+        	console.log("second for loop works", xhrData.dog_brands[i].types[j].type)
+        	// currentType = xhrData.dog_brands.types[j];
+        	// dogFoodString += `<div> ${currentType.type} </div>`
+        }
         dogFoodString += ``
         dogFoodString += ``
         dogFoodString += ``
-        dogFoodString += ``
-        dogFoodString += ``
-        dogFoodString += ``
-        dogFoodString += ``
-        console.log("this iteration works!", [i])
+        console.log("first for loop works", [i])
     }
 
     dogFoodContainer.innerHTML = dogFoodString;
